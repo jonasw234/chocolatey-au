18b6b13788637bad5184b836ddd39213a52d1cfd
Chocolatey <chocolatey@realdimensions.net>
2025-01-07 15:09:17 +0000

    AU: 1 updated - tachidesk-server [skip ci] https://gist.github.com/jonasw234/235ac14f598c6ee89f066ba309bdf5a7/a36defb45a1280d68a33e977e528e7096daa3125

--- tachidesk-server/tools/chocolateyInstall.ps1
+++ tachidesk-server/tools/chocolateyInstall.ps1
@@ -8,8 +8,8 @@ $packageArgs = @{
     url            = 'https://github.com/Suwayomi/Suwayomi-Server-preview/releases/download/v1.1.1-r1599/Suwayomi-Server-v1.1.1-r1599-windows-x86.msi'
     checksum       = '401797d4913edac889641c3db1fc885b70c97e0c9109600f8687404f1ec5df6f'
     checksumType   = 'sha256'
-    url64bit       = 'https://github.com/Suwayomi/Suwayomi-Server-preview/releases/download/v1.1.1-r1599/Suwayomi-Server-v1.1.1-r1599-windows-x64.msi'
-    checksum64     = '4b2b793d94d9c738126d43553a8baafd16884c1a2d936999ef0ea9239aaca057'
+    url64bit       = 'https://github.com/Suwayomi/Suwayomi-Server-preview/releases/download/v1.1.1-r1651/Suwayomi-Server-v1.1.1-r1651-windows-x64.msi'
+    checksum64     = 'fb8249500e8874e797b438a799d636504334357f36d7dad8cf87a233fdfb8445'
     checksumType64 = 'sha256'
     silentArgs     = '/qn /norestart'
 }
