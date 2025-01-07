5dd3fdff325fa4185f34ce9c537d92722d90761b
Jonas A. Wendorf <15037703+jonasw234@users.noreply.github.com>
2025-01-07 17:55:03 +0100

    feat: Update GoldenDict-ng

--- goldendict-ng/tools/chocolateyInstall.ps1
+++ goldendict-ng/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $packageName = $env:chocolateyPackageName
 $packageArgs = @{
     packageName  = $packageName
     fileType     = 'exe'
-    url          = 'https://github.com/xiaoyifang/goldendict-ng/releases/download/v24.05.05-LiXia.ecd1138c/6.7.0-GoldenDict-ng-Installer.exe'
-    checksum     = '4534a47f3d6fdb07fe656a469587f03e152d990a823ee85c6c5c191eb2c2ecbb'
+    url          = 'https://github.com/xiaoyifang/goldendict-ng/releases/download/v24.09.1-Release.ca9dd133/goldendict-Qt6.7.2.exe'
+    checksum     = 'f32247bc31e2766d728035f804b395c3f1e21a8856aa2d38a229145917c3c512'
     checksumType = 'sha256'
     silentArgs   = '/S'
 }
