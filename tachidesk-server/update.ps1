8648d75d73e6de6d589a1703a51fcba2776179c9
Jonas A. Wendorf <15037703+jonasw234@users.noreply.github.com>
2025-01-07 16:00:07 +0100

    fix: Suwayomi no longer provides x86 msis

--- tachidesk-server/update.ps1
+++ tachidesk-server/update.ps1
@@ -6,8 +6,6 @@ $preReleases = 'https://api.github.com/repos/Suwayomi/Suwayomi-Server-preview/re
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
             "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
             "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
@@ -31,24 +29,23 @@ function global:au_GetLatest {
     # Determine which release is more up-to-date based on release dates
     if ($stableReleaseDate -gt $preReleaseDate) {
         $latestRelease = $latestStableRelease
-    } else {
+    }
+    else {
         $latestRelease = $latestPreRelease
         $latestRelease.tag_name = $latestPreRelease.tag_name.split("-")[0] + ".1-" + $latestPreRelease.tag_name.split("-")[1] # Manually increment version string to indicate newer version
         $latestRelease.tag_name += "-rc" # Manually append `-rc` for pre-releases
     }
 
     # Extract URL of the release
-    $url32 = ($latestRelease.assets | Where-Object { $_.browser_download_url -match '-x86.msi$' }).browser_download_url
     $url64 = ($latestRelease.assets | Where-Object { $_.browser_download_url -match '-x64.msi$' }).browser_download_url
 
     # Cut first character from the version string
     $version = $latestRelease.tag_name.Substring(1)
 
     @{
-        URL32   = $url32
         URL64   = $url64
         Version = $version
     }
 }
 
-Update-Package -ChecksumFor All
+Update-Package -ChecksumFor 64
